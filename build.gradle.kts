1cb02c32f3080367b278f1ca05963a1953af7634
Heinrich Neb <heinrich.neb.extern@atruvia.de>
2026-04-16 14:54:36 -0700

    feat: add Maven Central publishing config with signing

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("jvm") version "2.0.0"
     `java-library`
     `maven-publish`
+    signing
 }
 
 group = "dev.cachly"
@@ -29,6 +30,11 @@ tasks.test {
     useJUnitPlatform()
 }
 
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
 publishing {
     publications {
         create<MavenPublication>("maven") {
@@ -43,8 +49,40 @@ publishing {
                         url.set("https://opensource.org/licenses/MIT")
                     }
                 }
+                developers {
+                    developer {
+                        id.set("cachly-dev")
+                        name.set("cachly.dev")
+                        email.set("hello@cachly.dev")
+                        url.set("https://cachly.dev")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/cachly-dev/sdk-kotlin.git")
+                    developerConnection.set("scm:git:ssh://github.com/cachly-dev/sdk-kotlin.git")
+                    url.set("https://github.com/cachly-dev/sdk-kotlin")
+                }
             }
         }
     }
+    repositories {
+        maven {
+            name = "central"
+            url = uri("https://central.sonatype.com/api/v1/publisher/upload")
+            credentials {
+                username = project.findProperty("centralUsername") as String? ?: System.getenv("CENTRAL_USERNAME") ?: ""
+                password = project.findProperty("centralPassword") as String? ?: System.getenv("CENTRAL_PASSWORD") ?: ""
+            }
+        }
+    }
+}
+
+signing {
+    val signingKey = project.findProperty("signingKey") as String? ?: System.getenv("GPG_SIGNING_KEY") ?: ""
+    val signingPassword = project.findProperty("signingPassword") as String? ?: System.getenv("GPG_SIGNING_PASSWORD") ?: ""
+    if (signingKey.isNotEmpty()) {
+        useInMemoryPgpKeys(signingKey, signingPassword)
+    }
+    sign(publishing.publications["maven"])
 }
 
